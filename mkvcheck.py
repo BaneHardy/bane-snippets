dace5e59a0bdacf67ebbe5ba5a3a5e93f8711f81
Bane Hardy <BaneHardy@GothamCity.com>
2020-10-02 16:20:09 +0200

    mkvcheck: port to python3 - find mkvinfo automatically - adjust regex for mkvinfo output so it works for versions before v32.0.0

--- mkvcheck.py
+++ mkvcheck.py
@@ -1,48 +1,56 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import re
+import shutil
 import subprocess
 import sys
 
-mkvinfo = '/usr/bin/mkvinfo'
 
 def get_size(mkv):
     try:
         size = os.path.getsize(mkv)
         return [size, size/1024/1024]
-    except:
+    except (FileNotFoundError, PermissionError):
         return [0, 0]
+
 
 def get_expected(mkv):
     try:
         output = subprocess.check_output([mkvinfo, '-z', mkv])
-    except:
+    except subprocess.CalledProcessError:
         return [0, 0]
-    pattern = re.compile('^\+ .* size (\d+) data size.*$')
+    pattern = re.compile(r'^\+ .* size (\d+)( data size \d+)?$')
     size = 0
-    for line in output.split('\n'):
+    for line in output.decode().split('\n'):
         match = pattern.search(line.strip())
         if match:
             size += int(match.group(1))
     return [size, size/1024/1024]
 
+
 def mkvcheck(mkv):
     size = get_size(mkv)
     expected = get_expected(mkv)
     if size[0] == 0 or expected[0] == 0:
-        print 'Error: "%s" %d/%d MiB' % (mkv, size[1], expected[1])
+        print('Error: {} {:.2f}/{:.2f} MiB'.format(mkv, size[1], expected[1]))
         return
     if size[0] == expected[0]:
-        print 'Good: "%s" %d/%d MiB' % (mkv, size[1], expected[1])
+        print('Good: {} {:.2f} MiB'.format(mkv, size[1]))
         return
     if size[0] != expected[0]:
-        print 'Bad: "%s" %d/%d MiB' % (mkv, size[1], expected[1])
+        print('Bad: {} {:.2f}/{:.2f} MiB'.format(mkv, size[1], expected[1]))
+
 
 if len(sys.argv) < 2:
-    print 'Error: Wrong number of arguments. Specify one or more files.'
-    print 'Usage: ./mkvcheck.py FILE [FILE ...]'
-    print 'Example: find . -iname "*.mkv" -print0 | xargs -0 ./mkvcheck.py'
+    print('Error: Wrong number of arguments. Specify one or more files.\n'
+          'Usage: ./mkvcheck.py FILE [FILE ...]\n'
+          'Example: find . -iname "*.mkv" -print0 | xargs -0 ./mkvcheck.py')
+    sys.exit(1)
+
+mkvinfo = shutil.which('mkvinfo')
+if not mkvinfo:
+    print('Error: mkvinfo not found. Install mkvtoolnix.')
     sys.exit(1)
 
 for arg in sys.argv[1:]:
