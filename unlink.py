6deb9bf6890e3bd67aec69b9bd6959c8d6baae2b
Bane Hardy <BaneHardy@GothamCity.com>
2020-10-30 22:01:09 +0100

    unlink: port to python3 - use absolute path instead of normpath

--- unlink.py
+++ unlink.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
 
 if len(sys.argv) < 2:
-    print 'Error: Wrong number of arguments. Specify one or more files or empty directories.'
-    print 'Usage: ./unlink.py FILE [FILE ...]'
-    print 'Deletes FILE but preserves atime and mtime of the parent directory.'
-    print 'Example: find /dir/ -name "file" -print0 | xargs -0 ./unlink.py'
-    print 'Example: find /dir/ -type d -empty -print0 | xargs -0 ./unlink.py'
+    print('Error: Wrong number of arguments. Specify one or more files or empty directories.')
+    print('Usage: ./unlink.py FILE [FILE ...]')
+    print('Deletes FILE but preserves atime and mtime of the parent directory.')
+    print('Example: find /dir/ -name "file" -print0 | xargs -0 ./unlink.py')
+    print('Example: find /dir/ -type d -empty -print0 | xargs -0 ./unlink.py')
     sys.exit(1)
 
 def unlink(filename):
     try:
-        filename = os.path.normpath(filename)
+        filename = os.path.abspath(filename)
         parent = os.path.dirname(filename)
         parent_stat = os.stat(parent)
         if os.path.isdir(filename):
@@ -22,7 +22,7 @@ def unlink(filename):
             os.unlink(filename)
         os.utime(parent, (parent_stat.st_atime, parent_stat.st_mtime))
     except:
-        print 'Error: "%s" caused problems.' % filename
+        print(f'Error: "{filename}" caused problems.')
 
 for arg in sys.argv[1:]:
     unlink(arg)
